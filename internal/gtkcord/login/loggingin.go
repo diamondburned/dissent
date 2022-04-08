2c4746350136048868166ea5e4d1f0d3f1370f05
diamondburned <diamond@arikawa-hi.me>
2022-04-07 23:33:00 -0700

    Remove useless code

--- internal/gtkcord/login/loggingin.go
+++
@@ -1,5 +0,0 @@
-package login
-
-// LoadingView is the view that's shown when the application is logging in. It
-// provides buttons for the user to invalidate the stored account entirely.
-type LoadingView struct{}
--- internal/gtkcord/login/login.go
+++
@@ -1,8 +0,0 @@
-package login
-
-type Manager interface {
-}
-
-// View describes a login widget that allows the user to login.
-type View struct {
-}
