c2e4917737efddb73fe06e0b2a4c819bd62bca74
diamondburned <diamond@arikawa-hi.me>
2022-04-19 14:52:32 -0700

    Bump gotk4-nix

--- .nix/src.nix
+++ .nix/src.nix
@@ -5,7 +5,7 @@ in {
 	gotk4-nix = systemPkgs.fetchFromGitHub {
 		owner = "diamondburned";
 		repo  = "gotk4-nix";
-		rev   = "17e14620e28322567f50664f1fa8ae7fee79d6fc";
-		hash  = "sha256:1mznaxchd3g9nq7scncfxa7cln44bkwr8sbkqbhb9lq06mf9cyjw";
+		rev   = "c8059c72e890b5128cb54f34eb659325031c7ec4";
+		hash  = "sha256:009n4vnjqcvfl147vqk3dgj28s3787v1b7l1p4hn3a1jyy70h9l2";
 	};
 }
