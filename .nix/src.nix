26dcac2d5725e96d5fca542b886d9e14a2ffdef5
diamondburned <diamond@arikawa-hi.me>
2022-04-19 01:57:34 -0700

    Lock gotk4-nix; add CI

--- .nix/src.nix
+++ .nix/src.nix
@@ -1,11 +1,11 @@
 let systemPkgs = import <nixpkgs> {};
 
 in {
-	gotk4-nix = ../../gotk4-nix;
-	# gotk4-nix = systemPkgs.fetchFromGitHub {
-	# 	owner = "diamondburned";
-	# 	repo  = "gotk4-nix";
-	# 	rev   = "4f407c20f8b07f4a87f0152fbefdc9a380042b83";
-	# 	hash  = "sha256:0zij5vbyjfbb2vda05vpvq268i7vx9bhzlbzzsa4zfzzr9427w66";
-	# };
+	# gotk4-nix = ../../gotk4-nix;
+	gotk4-nix = systemPkgs.fetchFromGitHub {
+		owner = "diamondburned";
+		repo  = "gotk4-nix";
+		rev   = "fd5b734f0914b560c1bfaa0b6bf24458648ef6b3";
+		hash  = "sha256:16axmj5wks0dxdvm9xwij1sm5s0y6c4pi329n4zndbw4l24p841h";
+	};
 }
