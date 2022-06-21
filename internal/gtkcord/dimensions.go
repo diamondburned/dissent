5cfcec3731b226d45dc0d6ce20bc64a069fbb7a6
diamondburned <diamond@arikawa-hi.me>
2022-06-20 19:53:05 -0700

    Add missing CSS variables

--- internal/gtkcord/dimensions.go
+++ internal/gtkcord/dimensions.go
@@ -35,15 +35,17 @@ var EmojiAttrs = textutil.Attrs(
 
 func init() {
 	cssutil.AddCSSVariables(map[string]string{
-		"header_height":       px(HeaderHeight),
-		"header_padding":      px(HeaderPadding),
-		"guild_icon_size":     px(GuildIconSize),
-		"channel_icon_size":   px(ChannelIconSize),
-		"message_avatar_size": px(MessageAvatarSize),
-		"embed_max_width":     px(EmbedMaxWidth),
-		"embed_img_height":    px(EmbedImgHeight),
-		"inline_emoji_size":   px(InlineEmojiSize),
-		"large_emoji_size":    px(LargeEmojiSize),
+		"header_height":        px(HeaderHeight),
+		"header_padding":       px(HeaderPadding),
+		"guild_icon_size":      px(GuildIconSize),
+		"channel_icon_size":    px(ChannelIconSize),
+		"message_avatar_size":  px(MessageAvatarSize),
+		"embed_max_width":      px(EmbedMaxWidth),
+		"embed_img_height":     px(EmbedImgHeight),
+		"inline_emoji_size":    px(InlineEmojiSize),
+		"large_emoji_size":     px(LargeEmojiSize),
+		"sticker_size":         px(StickerSize),
+		"user_bar_avatar_size": px(UserBarAvatarSize),
 	})
 }
 
