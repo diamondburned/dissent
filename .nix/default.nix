ae1b07502e15ddd6191c897d1baee0cd3fecb84a
diamondburned <diamond@arikawa-hi.me>
2022-07-23 23:44:19 -0700

    Bump GTK to v4.7.1

--- .nix/default.nix
+++ .nix/default.nix
@@ -1,7 +1,20 @@
 { action }:
 
 let src = import ./src.nix;
+	systemPkgs = import <nixpkgs> {};
 
 in import "${src.gotk4-nix}/${action}.nix" {
 	base = import ./base.nix;
+	pkgs = import "${src.gotk4-nix}/pkgs.nix" {
+		sourceNixpkgs = systemPkgs.fetchFromGitHub {
+			owner = "NixOS";
+			repo  = "nixpkgs";
+			rev   = "614a842";
+			hash  = "sha256:0gkpnjdcrh5s4jx0i8dc6679qfkffmz4m719aarzki4jss4l5n5p";
+		};
+		useFetched = true;
+		overlays = [
+			(import ./overlay.nix)
+		];
+	};
 }
