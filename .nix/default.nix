70728a8974e416fc44e72f72f3fb46e84681cb50
diamondburned <diamond@arikawa-hi.me>
2022-10-03 19:15:13 -0700

    Update and run svg2png.sh

--- .nix/default.nix
+++ .nix/default.nix
@@ -1,10 +1,23 @@
 { action }:
 
 let src = import ./src.nix;
+	base' = import ./base.nix;
 	systemPkgs = import <nixpkgs> {};
 
+	extraPkgs = {
+		shell = pkgs: with pkgs; [
+			imagemagick
+		];
+	};
+
+	base = base' // {
+		buildInputs = pkgs:
+			(base'.buildInputs pkgs) ++
+			((pkgs.lib.attrByPath [action] (pkgs: []) (extraPkgs)) pkgs);
+	};
+
 in import "${src.gotk4-nix}/${action}.nix" {
-	base = import ./base.nix;
+	base = base;
 	pkgs = import "${src.gotk4-nix}/pkgs.nix" {
 		sourceNixpkgs = systemPkgs.fetchFromGitHub {
 			owner = "NixOS";
