b908c49bf1942f74413979687fb45d8d34f2db09
milkknife <111794344+milkknife@users.noreply.github.com>
2023-02-01 23:12:50 +0100

    direct message icon - use papirus-icon-theme user-available

--- internal/gtkcord/sidebar/directbutton/button.go
+++ internal/gtkcord/sidebar/directbutton/button.go
@@ -27,7 +27,7 @@ var dmButtonCSS = cssutil.Applier("sidebar-dm-button-overlay", `
 func NewButton(ctx context.Context, open func()) *Button {
 	b := Button{ctx: ctx}
 
-	icon := gtk.NewImageFromFile("internal/icons/svg/logo.svg")
+	icon := gtk.NewImageFromFile("internal/icons/png/dm.png")
 	icon.SetIconSize(gtk.IconSizeLarge)
 	icon.SetPixelSize(gtkcord.GuildIconSize)
 
