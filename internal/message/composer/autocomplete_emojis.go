diamondburned/dissent

commit b18789ff4b60afefa3acb599e704430e22aaaedb

Put same-guild emojis first if we have no Nitro
This makes my life a bit easier.

internal/message/composer/autocomplete_emojis.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"html"
+	"sort"
 	"time"
 
 	"github.com/diamondburned/arikawa/v3/discord"
@@ -122,10 +123,10 @@ func (c *emojiCompleter) Search(ctx context.Context, str string) []autocomplete.
 		}
 	}
 
-	return c.search(str)
+	return c.search(str, hasNitro)
 }
 
-func (c *emojiCompleter) search(str string) []autocomplete.Data {
+func (c *emojiCompleter) search(str string, hasNitro bool) []autocomplete.Data {
 	res := fuzzy.FindFrom(str, c.emojis)
 	if len(res) > maxAutocompletion {
 		res = res[:maxAutocompletion]
@@ -134,6 +135,17 @@ func (c *emojiCompleter) search(str string) []autocomplete.Data {
 	data := c.matched[:0]
 	for _, r := range res {
 		data = append(data, c.emojis[r.Index])
+	}
+
+	// Put the guild emojis first if we don't have Nitro.
+	if !hasNitro {
+		sort.SliceStable(data, func(i, j int) bool {
+			a := data[i].(EmojiData)
+			b := data[j].(EmojiData)
+			correctA := a.Guild != nil && a.Guild.ID == c.guildID
+			correctB := b.Guild != nil && b.Guild.ID == c.guildID
+			return correctA && !correctB
+		})
 	}
 
 	return data

commit 2d2406b96e68c5ea0415f231e3946f77556961eb

Fix build error regression from b18789f

internal/message/composer/autocomplete_emojis.go
@@ -66,8 +66,11 @@ func (c *emojiCompleter) Search(ctx context.Context, str string) []autocomplete.
 
 	now := time.Now()
 
+	state := gtkcord.FromContext(ctx)
+	hasNitro := state.EmojiState.HasNitro()
+
 	if c.emojis != nil && c.updated.Add(emojiCacheExpiry).After(now) {
-		return c.search(str)
+		return c.search(str, hasNitro)
 	}
 
 	c.updated = now
@@ -85,16 +88,12 @@ func (c *emojiCompleter) Search(ctx context.Context, str string) []autocomplete.
 		})
 	}
 
-	state := gtkcord.FromContext(ctx)
-
 	var emojis []emoji.Guild
 	if showAllEmojis.Value() {
 		emojis, _ = state.EmojiState.AllEmojis()
 	} else {
 		emojis, _ = state.EmojiState.ForGuild(c.guildID)
 	}
-
-	hasNitro := state.EmojiState.HasNitro()
 
 	for i, guild := range emojis {
 		for _, emoji := range guild.Emojis {