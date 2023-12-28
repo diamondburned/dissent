5909cf3f51b1beffb0fa2ae8afc96e9aa5e25c98
diamondburned <diamond@arikawa-hi.me>
2023-12-28 00:03:32 -0800

    Fix composer placeholder not being left

--- internal/message/composer/composer.go
+++ internal/message/composer/composer.go
@@ -161,6 +161,7 @@ func NewView(ctx context.Context, ctrl Controller, chID discord.ChannelID) *View
 	v.Placeholder.AddCSSClass("composer-placeholder")
 	v.Placeholder.SetVAlign(gtk.AlignStart)
 	v.Placeholder.SetHAlign(gtk.AlignFill)
+	v.Placeholder.SetXAlign(0)
 	v.Placeholder.SetEllipsize(pango.EllipsizeEnd)
 
 	revealer := gtk.NewRevealer()
