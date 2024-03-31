18f670ff95b3eb2a9a9b806cef0ecfa845485778
diamondburned <diamond@libdb.so>
2024-03-31 16:43:00 -0700

    Switch to Nix Flake for shell

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+
+		gomod2nix = {
+			url = "github:nix-community/gomod2nix";
+			inputs.nixpkgs.follows = "nixpkgs";
+			inputs.flake-utils.follows = "flake-utils";
+		};
+
+		gotk4-nix = {
+			url = "github:diamondburned/gotk4-nix?ref=flake";
+			inputs.nixpkgs.follows = "nixpkgs";
+			inputs.gomod2nix.follows = "gomod2nix";
+			inputs.flake-utils.follows = "flake-utils";
+		};
+  };
+
+	outputs = { self, ... }@inputs:
+		let
+			base = import ./nix/base.nix // { src = self; };
+		in
+		(inputs.flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = inputs.nixpkgs.legacyPackages.${system}.appendOverlays [
+					inputs.gomod2nix.overlays.default
+					inputs.gotk4-nix.overlays.patchelf
+				];
+			in
+			{
+				devShells.default = inputs.gotk4-nix.lib.mkShell {
+					inherit base pkgs;
+					buildInputs = with pkgs; [
+						jq
+						niv
+						libxml2 # for xmllint
+						python3
+						gomod2nix
+						imagemagick
+						(callPackage ./.github/tools {})
+					];
+				};
+
+				packages.default = inputs.gotk4-nix.lib.mkPackage {
+					inherit base pkgs;
+					buildPkgs = pkgs;
+					version = self.rev or "unknown";
+				};
+			}
+		)) // {
+			lib.mkPackageCross =
+				{
+					system ? builtins.currentSystem,
+					version ? self.rev or "unknown",
+					...
+				}@args:
+				inputs.gotk4-nix.lib.mkPackageCross (args // {
+					inherit base;
+					pkgs = inputs.nixpkgs.legacyPackages.${system};
+				});
+		};
+}
