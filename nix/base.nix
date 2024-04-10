8064fad12f94ca1b335b188f09754d3f1eb05de9
diamondburned <diamond@libdb.so>
2024-04-10 16:23:05 -0700

    Only include icon directories in tarball

--- nix/base.nix
+++ nix/base.nix
@@ -1,9 +1,11 @@
+{ pkgs, src ? ../. }:
+
 {
+	inherit src;
+
 	pname = "dissent";
 	# 0000000000000000000000000000000000000000000000000000000000000000
 	# vendorSha256 = "10ijsv73bfgrsmvzirwv0nanyicxy6a6nayimif9dfvi9m5a7521";
-
-	src = ../.;
 	modules = ./gomod2nix.toml;
 
 	buildInputs = pkgs: with pkgs; [
@@ -28,7 +30,17 @@
 			path = ./so.libdb.dissent.service;
 		};
 		icons = {
-			path = ../internal/icons;
+			path = pkgs.linkFarm "dissent-icons" (map
+				(path: {
+					name = baseNameOf path;
+					inherit path;
+				})
+				[
+					../internal/icons/hicolor
+					../internal/icons/scalable
+					../internal/icons/symbolic
+				]
+			);
 		};
 	};
 }
